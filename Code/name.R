390b7212d58fd8672f9fb9d1c4f31309cefb1156
sleclare <sleclare1992@gmail.com>
2022-12-01 13:19:58 -0500

    detail hard reset

--- Code/name.R
+++ Code/name.R
@@ -32,6 +32,25 @@ summary(iris_pca)
 
 biplot(iris_pca)
 
+##Reverting to previous version
+
+# git reset --hard 43fb9cabf2581927c5ac7ad28e28690fa9a32286
+# HEAD is now at 43fb9ca Merge branch 'main' into bbaiser
+# 
+# C:\Users\biogirl92\OneDrive - University of Florida\Documents\Graduate Assistanship\Github\Fall_lab_2022_ben>git status
+# On branch main
+# Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.
+# (use "git pull" to update your local branch)
+# 
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+# .gitignore
+# Fall_lab_2022_ben.Rproj
+# Test_2.0.Rproj
+# 
+# nothing added to commit but untracked files present (use "git add" to track)
+# 
+# C:\Users\biogirl92\OneDrive - University of Florida\Documents\Graduate Assistanship\Github\Fall_lab_2022_ben>git push -f origin main
 
          
 
